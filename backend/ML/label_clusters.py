00c96eab5b47187755a59c1dcde98f4f885325be
aaryan <aaryankalia165@gmail.com>
2026-04-03 22:11:22 +0530

    Interpreted Clustered Data

--- backend/ML/label_clusters.py
+++ backend/ML/label_clusters.py
@@ -15,5 +15,21 @@ print(cluster_summary)
 
 # Save
 cluster_summary.to_csv("cluster_summary.csv")
-
 print("\nSaved: cluster_summary.csv")
+
+# Interpreted Cluster
+label_map = {
+    0: "High-Risk Independents",
+    1: "Core Market",
+    2: "Anomalous Outliers",
+    3: "Speculative Rockets",
+    4: "Market Followers"
+}
+
+df["cluster_label"] = df["cluster_id"].map(label_map)
+
+df[["coin_symbol", "cluster_id", "cluster_label"]].to_csv(
+    "genome_clusters_labeled.csv", index=False
+)
+
+print("\nSaved: genome_clusters_labeled.csv")
