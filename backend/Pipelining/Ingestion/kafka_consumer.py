42b460b1a7160f31b4c7fa47774cc8d75b3535b6
aaryan <aaryankalia165@gmail.com>
2026-04-18 20:04:25 +0530

    Create Kafka Comsumer file and completed data send/recieve part

---
+++ backend/Pipelining/Ingestion/kafka_consumer.py
@@ -0,0 +1,32 @@
+import json
+from kafka import KafkaConsumer
+
+
+def create_consumer(topic: str):
+    return KafkaConsumer(
+        topic,
+        bootstrap_servers="localhost:9092",
+        auto_offset_reset="earliest",
+        enable_auto_commit=True,
+        group_id="crypto-group",
+        value_deserializer=lambda x: json.loads(x.decode("utf-8"))
+    )
+
+
+def consume_data(consumer):
+    print("Consumer started...\n")
+
+    for message in consumer:
+        data = message.value
+        print(f"Received: {data['coin_symbol']}")
+
+
+def main():
+    topic = "crypto_genome"
+
+    consumer = create_consumer(topic)
+    consume_data(consumer)
+
+
+if __name__ == "__main__":
+    main()
