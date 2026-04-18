3613e0f1a5ec10381d64a717bf66788b1e149653
aaryan <aaryankalia165@gmail.com>
2026-04-19 00:31:22 +0530

    Update kafka_consumer.py and output file is generated

--- backend/Pipelining/Ingestion/kafka_consumer.py
+++ backend/Pipelining/Ingestion/kafka_consumer.py
@@ -1,5 +1,9 @@
 import json
+import os
+import pandas as pd
 from kafka import KafkaConsumer
+
+OUTPUT_PATH = "../Storage/stream_output.csv"
 
 
 def create_consumer(topic: str):
@@ -13,16 +17,41 @@ def create_consumer(topic: str):
     )
 
 
+def initialize_storage():
+
+    os.makedirs(os.path.dirname(OUTPUT_PATH), exist_ok=True)
+    
+    if not os.path.exists(OUTPUT_PATH):
+        df = pd.DataFrame(columns=[
+            "coin_symbol",
+            "timestamp"
+        ])
+        df.to_csv(OUTPUT_PATH, index=False)
+
+
+def write_record(data):
+    df = pd.DataFrame([{
+        "coin_symbol": data["coin_symbol"],
+        "timestamp": pd.Timestamp.now()
+    }])
+    df.to_csv(OUTPUT_PATH, mode="a", header=False, index=False)
+
+
 def consume_data(consumer):
-    print("Consumer started...\n")
+    print("Consumer + Storage started...\n")
 
     for message in consumer:
         data = message.value
-        print(data)
+
+        write_record(data)
+
+        print(f"Stored: {data['coin_symbol']}")
 
 
 def main():
     topic = "crypto_genome"
+
+    initialize_storage()
 
     consumer = create_consumer(topic)
     consume_data(consumer)
