e8a765558c2e8093126de6ed8ed88c509e1314f1
aaryan <aaryankalia165@gmail.com>
2026-04-21 12:12:00 +0530

    Changes in DAG

--- backend/Pipelining/Airflow/dags/crypto_pipeline_dag.py
+++ backend/Pipelining/Airflow/dags/crypto_pipeline_dag.py
@@ -2,16 +2,6 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
 import subprocess
-import sys
-import os
-
-sys.path.append(
-    os.path.abspath(
-        os.path.join(os.path.dirname(__file__), '../../Warehouse')
-    )
-)
-
-from load_to_postgres import load_to_postgres_function
 
 def run_producer():
     subprocess.run(["python", "../../Ingestion/kafka_producer.py"])
@@ -23,25 +13,25 @@ def run_loader():
     subprocess.run(["python", "../../Warehouse/load_to_postgres.py"])
 
 with DAG(
-    dag_id="crypto_genome_pipeline",
+    dag_id='crypto_genome_pipeline',
     start_date=datetime(2024, 1, 1),
     schedule_interval=None,
     catchup=False
 ) as dag:
 
-    producer_task = PythonOperator(
-        task_id="run_producer",
+    producer = PythonOperator(
+        task_id='run_producer',
         python_callable=run_producer
     )
 
-    consumer_task = PythonOperator(
-        task_id="run_consumer",
+    consumer = PythonOperator(
+        task_id='run_consumer',
         python_callable=run_consumer
     )
 
-    run_loader = PythonOperator(
-    task_id='run_loader',
-    python_callable=load_to_postgres_function
+    loader = PythonOperator(
+        task_id='run_loader',
+        python_callable=run_loader
     )
 
-    producer_task >> consumer_task >> run_loader
+    producer >> consumer >> loader
