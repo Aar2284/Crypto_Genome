f1a81f99a7b2c02e0db01cd754b828c375ecc4e2
aaryan <aaryankalia165@gmail.com>
2026-04-23 23:44:57 +0530

    Upgraded the DAG Code

--- backend/Pipelining/Airflow/dags/crypto_pipeline_dag.py
+++ backend/Pipelining/Airflow/dags/crypto_pipeline_dag.py
@@ -2,6 +2,7 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
 import subprocess
+import psycopg2
 
 def run_producer():
     print("Producer started...")
@@ -18,6 +19,25 @@ def run_loader():
     subprocess.run(["python", "/opt/airflow/project/Warehouse/load_to_postgres.py"])
     print("Data inserted successfully")
 
+def validate_data():
+    print("Checking data in PostgreSQL...")
+
+    conn = psycopg2.connect(
+        host="host.docker.internal",
+        database="crypto",
+        user="admin",
+        password="admin"
+    )
+
+    cur = conn.cursor()
+    cur.execute("SELECT COUNT(*) FROM crypto_stream;")
+
+    count = cur.fetchone()[0]
+
+    print(f"Total rows in DB: {count}")
+
+    conn.close()
+
 with DAG(
     dag_id='crypto_genome_pipeline',
     start_date=datetime(2024, 1, 1),
@@ -28,5 +48,6 @@ with DAG(
     producer = PythonOperator(task_id='run_producer', python_callable=run_producer)
     consumer = PythonOperator(task_id='run_consumer', python_callable=run_consumer)
     loader = PythonOperator(task_id='run_loader', python_callable=run_loader)
+    validate = PythonOperator(task_id='validate_data',python_callable=validate_data)
 
-    producer >> consumer >> loader
+    producer >> consumer >> loader >> validate
